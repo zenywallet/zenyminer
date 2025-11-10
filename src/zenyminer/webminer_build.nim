b27e3e330ffb21654fb820e9bc25a745ec5b047a
zenywallet <zenywallet@bitzeny.jp>
2025-11-11 08:46:19 +0900

    change webminer patch

--- src/zenyminer/webminer_build.nim
+++ src/zenyminer/webminer_build.nim
@@ -2,6 +2,7 @@
 
 import std/os
 import std/osproc
+import regex
 import emsdkenv
 
 const srcDir = currentSourcePath().parentDir()
@@ -20,13 +21,21 @@ template withDir(dir: string; body: untyped): untyped =
   finally:
     setCurrentDir(curDir)
 
+proc patch(file: string) =
+  var s = readFile(file)
+  s = s.replace(re2"""class\s+ExitStatus\s*\{\s*name\s*=\s*"ExitStatus"\s*;\s*constructor\s*\(\s*status\s*\)\s*\{""",
+    """class ExitStatus {
+  constructor(status) {
+    this.name = "ExitStatus";""")
+  writeFile(file, s)
+
 withDir srcDir:
   exec "nim js -d:release -o:webminer_loader.js webminer_loader.nim"
   exec "nim js -d:release -d:nodejs -o:webminer_externs.js webminer_externs.nim"
   emsdkEnv "nim c -d:release -d:emscripten -o:miner.js_tmp --mm:orc webminer.nim"
   emsdkEnv "nim c -d:release -d:emscripten -d:ENABLE_SIMD128 -o:miner-simd128.js_tmp --mm:orc webminer.nim"
-  exec "nim c -r webminer_patch.nim"
-  exec "rm webminer_patch"
+  patch("miner.js_tmp")
+  patch("miner-simd128.js_tmp")
   exec """
 if [ -x "$(command -v google-closure-compiler)" ]; then
   closure_compiler="google-closure-compiler"
