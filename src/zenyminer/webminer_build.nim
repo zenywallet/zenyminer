902bce6e49fc16728a32580674f0542cdf95d188
zenywallet <zenywallet@bitzeny.jp>
2025-11-13 00:31:38 +0900

    change webminer build zenyjs minify

--- src/zenyminer/webminer_build.nim
+++ src/zenyminer/webminer_build.nim
@@ -4,6 +4,7 @@ import std/os
 import std/osproc
 import regex
 import emsdkenv
+import zenyjs/exec
 
 const srcDir = currentSourcePath().parentDir()
 
@@ -60,32 +61,20 @@ template withDir(dir: string; body: untyped): untyped =
   finally:
     setCurrentDir(curDir)
 
-proc patch(file: string) =
+proc patch(file: string): string =
   var s = readFile(file)
   s = s.replace(re2"""class\s+ExitStatus\s*\{\s*name\s*=\s*"ExitStatus"\s*;\s*constructor\s*\(\s*status\s*\)\s*\{""",
     """class ExitStatus {
   constructor(status) {
     this.name = "ExitStatus";""")
-  writeFile(file, s)
+  s
 
 withDir srcDir:
   exec "nim js -d:release -o:webminer_loader.js webminer_loader.nim"
-  writeFile("webminer_externs.js", externs)
   emsdkEnv "nim c -d:release -d:emscripten -o:miner.js_tmp --mm:orc webminer.nim"
   emsdkEnv "nim c -d:release -d:emscripten -d:ENABLE_SIMD128 -o:miner-simd128.js_tmp --mm:orc webminer.nim"
-  patch("miner.js_tmp")
-  patch("miner-simd128.js_tmp")
-  exec """
-if [ -x "$(command -v google-closure-compiler)" ]; then
-  closure_compiler="google-closure-compiler"
-else
-  closure_compiler="java -jar $(ls closure-compiler-*.jar | sort -r | head -n1)"
-fi
-echo "use $closure_compiler"
-$closure_compiler --compilation_level ADVANCED --jscomp_off=checkVars --jscomp_off=checkTypes --jscomp_off=uselessCode --js_output_file=miner.js --externs webminer_externs.js miner.js_tmp 2>&1 | cut -c 1-240
-$closure_compiler --compilation_level ADVANCED --jscomp_off=checkVars --jscomp_off=checkTypes --jscomp_off=uselessCode --js_output_file=miner-simd128.js --externs webminer_externs.js miner-simd128.js_tmp 2>&1 | cut -c 1-240
-"""
+  writeFile("miner.js", minifyJsCode(srcDir, patch("miner.js_tmp"), externs))
+  writeFile("miner-simd128.js", minifyJsCode(srcDir, patch("miner-simd128.js_tmp"), externs))
   exec "rm miner-simd128.js_tmp"
   exec "rm miner.js_tmp"
-  exec "rm webminer_externs.js"
   exec "rm webminer_loader.js"
