0d767255cdd3b288212082b6bd1cb9af6457dd73
zenywallet <zenywallet@bitzeny.jp>
2026-03-03 00:07:54 +0900

    change mining miner except

--- src/zenyminer/mining.nim
+++ src/zenyminer/mining.nim
@@ -129,160 +129,166 @@ proc main*() =
   var rawBlock: Array[byte]
   var target: Array[byte]
 
-  while not abort:
-    var message = messageChannel[].recv()
-    if message.cmd == MessageCmd.FindBlock:
-      if blockId == message.blockId:
-        if rawBlock.len > 80:
-          var findBlock = (message.header, rawBlock[80..^1]).toBytes
-          var retSubmit = submitBlock.send($findBlock)
-          echo "find block - ", message.blockHash
-          if retSubmit["error"].kind == JNull:
-            if prevFoundBlockId == message.blockId:
-              echo "submit success (already submitted the same block height)"
+  try:
+    while not abort:
+      var message = messageChannel[].recv()
+      if message.cmd == MessageCmd.FindBlock:
+        if blockId == message.blockId:
+          if rawBlock.len > 80:
+            var findBlock = (message.header, rawBlock[80..^1]).toBytes
+            var retSubmit = submitBlock.send($findBlock)
+            echo "find block - ", message.blockHash
+            if retSubmit["error"].kind == JNull:
+              if prevFoundBlockId == message.blockId:
+                echo "submit success (already submitted the same block height)"
+              else:
+                echo "submit success"
+                prevFoundBlockId = message.blockId
+                inc(statsFind)
+                updateBlockTemplate = true
             else:
-              echo "submit success"
-              prevFoundBlockId = message.blockId
-              inc(statsFind)
-              updateBlockTemplate = true
-          else:
-            echo "submit failed ", retSubmit
-      else:
-        echo "stale block - ", message.blockHash
-
-    elif message.cmd == MessageCmd.UpdateBlock:
-      updateBlockTemplate = true
-
-    if updateBlockTemplate:
-      var retTmpl = getBlockTemplate.send(blockTemplateParam)
-      if retTmpl["error"].kind != JNull:
-        echo "get template failed ", retTmpl
-        doAbort()
-        break
-      var blockTmpl = retTmpl["result"]
-      prevHash = blockTmpl["previousblockhash"].getStr.Hex.toBlockHash
-      var transactions = blockTmpl["transactions"]
-      txCount = transactions.len.uint32
-      var curTime, prevCurTime: uint32
-      curTime = blockTmpl["curtime"].getInt.uint32
-      if txCount != prevTxCount or prevHash != prevPrevHash:
-        prevTxCount = txCount
-        prevPrevHash = prevHash
-        target = blockTmpl["target"].getStr.Hex.toHash.toBytes
-        var height = blockTmpl["height"].getInt.uint32
-        var coinBaseValue = blockTmpl["coinbasevalue"].getBiggestInt.uint64
-        var sig = (@^[byte 3'u8], height.toBytes[0..2]).toBytes
-        var witnessFlag = false
-        for t in transactions:
-          if t["txid"].getStr != t["hash"].getStr and t["data"].getStr[8..11].Hex.toBytes.toUint16BE == 1'u16:
-            witnessFlag = true
-            break
-        var txo = newTx()
-        var tx = txo.handle
-        tx.ver = 1'i32
-        tx.flags = Flags(0'u8)
-        tx.ins = @^[TxIn (tx: Hash(pad(32)), n: 0xffffffff'u32, sig: Sig(sig), sequence: 0xffffffff'u32)]
-        if witnessFlag:
-          var witnessCommitmentScript: Array[byte]
-          if blockTmpl.hasKey("default_witness_commitment"):
-            witnessCommitmentScript = blockTmpl["default_witness_commitment"].getStr.Hex.toBytes
-          else:
-            var txHashes: Array[Array[byte]]
-            txHashes.add(witnessReserved)
-            for t in transactions:
-              txHashes.add(t["hash"].getStr.Hex.toHash.toBytes)
-            let witnessRootHash = merkle(txHashes)
-            let witnessCommitmentHash = sha256d((witnessRootHash, witnessReserved).toBytes).toBytes
-            let witnessCommitment = (WitnessCommitmentHeader, witnessCommitmentHash).toBytes
-            witnessCommitmentScript = (OP_RETURN, PushData(witnessCommitment)).toBytes
-          tx.outs = @^[TxOut (value: 0'u64, script: Script(witnessCommitmentScript)),
-                    (value: coinBaseValue, script: Script(myAddressScript))]
+              echo "submit failed ", retSubmit
         else:
-          tx.outs = @^[TxOut (value: coinBaseValue, script: Script(myAddressScript))]
-        tx.locktime = 0
-        var txid = txo.txid
+          echo "stale block - ", message.blockHash
 
-        var txids: Array[Array[byte]]
-        txids.add(cast[Array[byte]](txid))
-        for t in transactions:
-          txids.add(t["txid"].getStr.Hex.toHash.toBytes)
+      elif message.cmd == MessageCmd.UpdateBlock:
+        updateBlockTemplate = true
 
-        var header = new BlockHeader
-        header.ver = blockTmpl["version"].getInt.int32
-        header.prev = prevHash
-        header.merkle = merkle(txids)
-        header.time = curTime
-        header.bits = blockTmpl["bits"].getStr.Hex.toBytes.toReverse.toUint32
-        header.nonce = 0'u32
-
-        var blk = new Block
-        blk.header = header
-        blk.txn = VarInt(1 + txCount)
-        blk.txs.add(txo)
-
-        var txdatas: Array[byte]
-        for t in transactions:
-          txdatas.add(t["data"].getStr.Hex.toBytes)
-
-        rawBlock = (blk, txdatas).toBytes
-
-        inc(blockId)
-        var nonceBase: uint32
-        discard cryptSeed(cast[ptr UncheckedArray[byte]](addr nonceBase), 4)
-        var shift = MINER_THREAD_NUM * (shiftCount mod 2).int
-        for i in 0..<MINER_THREAD_NUM:
-          let pdata = addr minerDatas[][i + shift]
-          pdata[].header = cast[ptr BlockHeaderObj](addr rawBlock[0])[]
-          pdata[].target = cast[ptr array[32, byte]](addr target[0])[]
-          pdata[].blockId = blockId
-          var nonce = nonceBase + (i * (uint32.high.int / MINER_THREAD_NUM).int).uint32
-          copyMem(addr pdata[].header.nonce, addr nonce, 4)
-          minerParams[][i].data = pdata
-        inc(shiftCount)
-        statsTimeStart = 0.0
-        echo "new block template #", height
-      else:
-        if curTime != prevCurTime:
-          prevCurTime = curTime
-          if shiftCount mod 2 == 0:
-            for i in 0..<MINER_THREAD_NUM:
-              let pdata = addr minerDatas[][i]
-              pdata[] = minerDatas[][i + MINER_THREAD_NUM]
-              copyMem(addr pdata[].header.time, addr curTime, 4)
-              minerParams[][i].data = pdata
+      if updateBlockTemplate:
+        var retTmpl = getBlockTemplate.send(blockTemplateParam)
+        if retTmpl["error"].kind != JNull:
+          echo "get template failed ", retTmpl
+          doAbort()
+          break
+        var blockTmpl = retTmpl["result"]
+        prevHash = blockTmpl["previousblockhash"].getStr.Hex.toBlockHash
+        var transactions = blockTmpl["transactions"]
+        txCount = transactions.len.uint32
+        var curTime, prevCurTime: uint32
+        curTime = blockTmpl["curtime"].getInt.uint32
+        if txCount != prevTxCount or prevHash != prevPrevHash:
+          prevTxCount = txCount
+          prevPrevHash = prevHash
+          target = blockTmpl["target"].getStr.Hex.toHash.toBytes
+          var height = blockTmpl["height"].getInt.uint32
+          var coinBaseValue = blockTmpl["coinbasevalue"].getBiggestInt.uint64
+          var sig = (@^[byte 3'u8], height.toBytes[0..2]).toBytes
+          var witnessFlag = false
+          for t in transactions:
+            if t["txid"].getStr != t["hash"].getStr and t["data"].getStr[8..11].Hex.toBytes.toUint16BE == 1'u16:
+              witnessFlag = true
+              break
+          var txo = newTx()
+          var tx = txo.handle
+          tx.ver = 1'i32
+          tx.flags = Flags(0'u8)
+          tx.ins = @^[TxIn (tx: Hash(pad(32)), n: 0xffffffff'u32, sig: Sig(sig), sequence: 0xffffffff'u32)]
+          if witnessFlag:
+            var witnessCommitmentScript: Array[byte]
+            if blockTmpl.hasKey("default_witness_commitment"):
+              witnessCommitmentScript = blockTmpl["default_witness_commitment"].getStr.Hex.toBytes
+            else:
+              var txHashes: Array[Array[byte]]
+              txHashes.add(witnessReserved)
+              for t in transactions:
+                txHashes.add(t["hash"].getStr.Hex.toHash.toBytes)
+              let witnessRootHash = merkle(txHashes)
+              let witnessCommitmentHash = sha256d((witnessRootHash, witnessReserved).toBytes).toBytes
+              let witnessCommitment = (WitnessCommitmentHeader, witnessCommitmentHash).toBytes
+              witnessCommitmentScript = (OP_RETURN, PushData(witnessCommitment)).toBytes
+            tx.outs = @^[TxOut (value: 0'u64, script: Script(witnessCommitmentScript)),
+                      (value: coinBaseValue, script: Script(myAddressScript))]
           else:
-            for i in 0..<MINER_THREAD_NUM:
-              let pdata = addr minerDatas[][i + MINER_THREAD_NUM]
-              pdata[] = minerDatas[][i]
-              copyMem(addr pdata[].header.time, addr curTime, 4)
-              minerParams[][i].data = pdata
-          inc(shiftCount)
+            tx.outs = @^[TxOut (value: coinBaseValue, script: Script(myAddressScript))]
+          tx.locktime = 0
+          var txid = txo.txid
 
-      template statsStart() {.dirty.} =
-        statsTimeStart = epochTime()
-        for i in 0..<MINER_THREAD_NUM:
-          statsCountStart[i] = minerParams[][i].data[].header.nonce
+          var txids: Array[Array[byte]]
+          txids.add(cast[Array[byte]](txid))
+          for t in transactions:
+            txids.add(t["txid"].getStr.Hex.toHash.toBytes)
 
-      template statsEnd() {.dirty.} =
-        statsTimeEnd = epochTime()
-        for i in 0..<MINER_THREAD_NUM:
-          statsCountEnd[i] = minerParams[][i].data[].header.nonce
+          var header = new BlockHeader
+          header.ver = blockTmpl["version"].getInt.int32
+          header.prev = prevHash
+          header.merkle = merkle(txids)
+          header.time = curTime
+          header.bits = blockTmpl["bits"].getStr.Hex.toBytes.toReverse.toUint32
+          header.nonce = 0'u32
 
-      if statsTimeStart > 0:
-        if epochTime() - statsTimeStart > 5.0:
-          statsEnd()
-          var elapsed = statsTimeEnd - statsTimeStart
-          var hashRateStr: string = fmt"Found:{statsFind} "
-          var ths: float
+          var blk = new Block
+          blk.header = header
+          blk.txn = VarInt(1 + txCount)
+          blk.txs.add(txo)
+
+          var txdatas: Array[byte]
+          for t in transactions:
+            txdatas.add(t["data"].getStr.Hex.toBytes)
+
+          rawBlock = (blk, txdatas).toBytes
+
+          inc(blockId)
+          var nonceBase: uint32
+          discard cryptSeed(cast[ptr UncheckedArray[byte]](addr nonceBase), 4)
+          var shift = MINER_THREAD_NUM * (shiftCount mod 2).int
           for i in 0..<MINER_THREAD_NUM:
-            var hs = (statsCountEnd[i] - statsCountStart[i]).float / elapsed
-            ths = ths + hs
-            hashRateStr.add(fmt"{i}:{hs:.3f} ")
-          hashRateStr.add(fmt"Total:{ths:.3f} H/s")
-          echo hashRateStr
+            let pdata = addr minerDatas[][i + shift]
+            pdata[].header = cast[ptr BlockHeaderObj](addr rawBlock[0])[]
+            pdata[].target = cast[ptr array[32, byte]](addr target[0])[]
+            pdata[].blockId = blockId
+            var nonce = nonceBase + (i * (uint32.high.int / MINER_THREAD_NUM).int).uint32
+            copyMem(addr pdata[].header.nonce, addr nonce, 4)
+            minerParams[][i].data = pdata
+          inc(shiftCount)
+          statsTimeStart = 0.0
+          echo "new block template #", height
+        else:
+          if curTime != prevCurTime:
+            prevCurTime = curTime
+            if shiftCount mod 2 == 0:
+              for i in 0..<MINER_THREAD_NUM:
+                let pdata = addr minerDatas[][i]
+                pdata[] = minerDatas[][i + MINER_THREAD_NUM]
+                copyMem(addr pdata[].header.time, addr curTime, 4)
+                minerParams[][i].data = pdata
+            else:
+              for i in 0..<MINER_THREAD_NUM:
+                let pdata = addr minerDatas[][i + MINER_THREAD_NUM]
+                pdata[] = minerDatas[][i]
+                copyMem(addr pdata[].header.time, addr curTime, 4)
+                minerParams[][i].data = pdata
+            inc(shiftCount)
+
+        template statsStart() {.dirty.} =
+          statsTimeStart = epochTime()
+          for i in 0..<MINER_THREAD_NUM:
+            statsCountStart[i] = minerParams[][i].data[].header.nonce
+
+        template statsEnd() {.dirty.} =
+          statsTimeEnd = epochTime()
+          for i in 0..<MINER_THREAD_NUM:
+            statsCountEnd[i] = minerParams[][i].data[].header.nonce
+
+        if statsTimeStart > 0:
+          if epochTime() - statsTimeStart > 5.0:
+            statsEnd()
+            var elapsed = statsTimeEnd - statsTimeStart
+            var hashRateStr: string = fmt"Found:{statsFind} "
+            var ths: float
+            for i in 0..<MINER_THREAD_NUM:
+              var hs = (statsCountEnd[i] - statsCountStart[i]).float / elapsed
+              ths = ths + hs
+              hashRateStr.add(fmt"{i}:{hs:.3f} ")
+            hashRateStr.add(fmt"Total:{ths:.3f} H/s")
+            echo hashRateStr
+            statsStart()
+        else:
           statsStart()
-      else:
-        statsStart()
+
+  except:
+    let e = getCurrentException()
+    echo e.name, ": ", e.msg
+    doAbort()
 
   for i in 0..<MINER_THREAD_NUM:
     minerParams[][i].abort = true
