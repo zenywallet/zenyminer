2f80ea33d257e9090c5021ff4a284ae36bb53823
zenywallet <zenywallet@bitzeny.jp>
2026-03-05 00:12:31 +0900

    add web mining default networks

--- src/zenyminer/webmining.nim
+++ src/zenyminer/webmining.nim
@@ -15,6 +15,9 @@ import os, macros
 const WEBSOCKET_PROTOCOL = "deoxy-0.1"
 const WEBSOCKET_ENTRY_URL = "wss://localhost:8000/ws"
 
+import zenyjs/config
+networksDefault()
+
 import zenyjs/address
 include karax / prelude
 
